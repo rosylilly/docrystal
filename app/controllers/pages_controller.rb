a5b194394abda42f457ba4960df131795dcb4566
Sho Kusano <rosylilly@aduca.org>
2015-07-10 15:46:57 +0900

    Fix search style

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def root(q = nil)
     if !q.nil? && q.present?
-      @packages = Package.search(q).records
+      @packages = Package.search(query: { match: { path: q } }).records
     else
       @packages = Package.order(updated_at: :desc).limit(20)
     end
