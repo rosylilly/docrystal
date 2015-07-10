8a4d5aff27134def6d83eefc6550c5d91de6ff80
Sho Kusano <rosylilly@aduca.org>
2015-07-10 14:39:01 +0900

    Fix bug

--- app/jobs/crystal_doc_job.rb
+++ app/jobs/crystal_doc_job.rb
@@ -8,6 +8,9 @@ class CrystalDocJob < ActiveJob::Base
   def perform(doc_id)
     @doc = Package::Doc.find_by(id: doc_id)
 
+    return unless @doc
+
+    logger.info(working_dir)
     FileUtils.mkdir_p(working_dir)
 
     git('clone', @doc.package.github_repository.git_url, working_dir)
@@ -17,6 +20,7 @@ class CrystalDocJob < ActiveJob::Base
 
       FileUtils.rm_rf(working_dir.join('doc'))
 
+      crystal('deps', 'install')
       crystal('docs')
 
       Dir['doc/**/*'].each do |file|
@@ -32,7 +36,7 @@ class CrystalDocJob < ActiveJob::Base
     end
 
     @doc.touch(:generated_at)
-    Pusher["doc-#{@doc.sha}"].trigger('generated', {})
+    Pusher["doc-#{@doc.sha}"].trigger('generated', {}) if Pusher.key
   ensure
     if @doc && File.directory?(working_dir)
       FileUtils.rm_rf(working_dir)
@@ -41,11 +45,11 @@ class CrystalDocJob < ActiveJob::Base
   end
 
   def working_dir
-    @working_dir ||= Rails.root.join('tmp', 'crystal-doc', Digest::SHA1.hexdigest("#{@doc.id}-#{Time.current.to_i}"))
+    @working_dir ||= Rails.root.join('tmp', 'crystal-doc', Digest::SHA1.hexdigest("#{@doc.id}-#{Time.current.to_f}"))
   end
 
   def git(cmd, *args)
-    Open3.popen3("git #{cmd} #{args.join(' ')}") do |input, stdout, stderr, wait|
+    Open3.popen3("git #{cmd} #{args.join(' ')}", chdir: working_dir) do |input, stdout, stderr, wait|
       input.close
 
       stdout.each do |line|
@@ -53,7 +57,7 @@ class CrystalDocJob < ActiveJob::Base
       end
 
       stderr.each do |line|
-        logger.error(line)
+        logger.warn(line)
       end
 
       wait.value
@@ -61,7 +65,7 @@ class CrystalDocJob < ActiveJob::Base
   end
 
   def crystal(cmd, *args)
-    Open3.popen3("crystal #{cmd} #{args.join(' ')}") do |input, stdout, stderr, wait|
+    Open3.popen3("crystal #{cmd} #{args.join(' ')}", chdir: working_dir) do |input, stdout, stderr, wait|
       input.close
 
       stdout.each do |line|
