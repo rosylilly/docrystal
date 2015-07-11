4d61caf1efebb21c0df60ef02b015f0dde154783
Sho Kusano <rosylilly@aduca.org>
2015-07-12 01:21:31 +0900

    Fix cache logger

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,5 +1,5 @@
 stack = Faraday::RackBuilder.new do |builder|
-  builder.use Faraday::HttpCache, store: Rails.cache
+  builder.use Faraday::HttpCache, store: Rails.cache, serializer: Oj, logger: Rails.logger
   builder.use Octokit::Response::RaiseError
   builder.adapter Faraday.default_adapter
 end
